3d73c4d6b302e2d50147261e2204a06409f1e25f
raghava reddy <raghavareddy1515@gmail.com>
2021-12-13 22:07:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoPush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm in Training");
+            Console.WriteLine("I'm raghava reddy");
+        }
+    }
+}
